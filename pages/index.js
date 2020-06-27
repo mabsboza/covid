2c9ca0cb34c310c6e14e60b8464b4f10bb349178
mabsboza <marcosboza@moi-solutions.com>
2020-06-27 14:30:25 -0600

    Update pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Layout from '../layout/layout'
 import Header from '../containers/header'
+import Main from '../containers/main'
 
 const index = () => {
   return (
     <Layout>
       <Header />
+      <Main />
     </Layout>
   );
 };
